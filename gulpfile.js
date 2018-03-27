71f782fa8f9dc9f4e97497300162a453f52abdc3
Chris Halliwell <chris.halliwell@saatchi.co.uk>
2018-03-27 18:05:30 +0100

    init: Site Build and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssimport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log("Hurray!");
@@ -15,10 +16,8 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
-
-
 });
 
 gulp.task('watch', function(){
